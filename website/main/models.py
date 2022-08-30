b576c1854488ef4f7a0a51e7259de316c7c5aa29
yewon <yewon.ux@gmail.com>
2022-08-30 22:53:57 +0900

    set admin

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=50, default = '')
+    lat = models.FloatField(default = '')
+    lan = models.FloatField(default = '')
+    location = models.CharField(max_length=50, default = '')
+    content = models.TextField()
+    
+    # admin에서 이름으로 출력되게 변경
+    def __str__(self):
+        return self.name
