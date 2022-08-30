27ea699991620145ba5bca68ec4766c45b4bde29
yewon <yewon.ux@gmail.com>
2022-08-31 00:37:39 +0900

    model to json

--- website/main/models.py
+++ website/main/models.py
@@ -6,6 +6,7 @@ class Place(models.Model):
     lat = models.FloatField(default = '')
     lan = models.FloatField(default = '')
     location = models.CharField(max_length=50, default = '')
+    marker = models.CharField(max_length=15, default = '')
     content = models.TextField()
     
     # admin에서 이름으로 출력되게 변경
--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,15 @@
+from unicodedata import name
 from django.shortcuts import render
+from .models import Place
+from django.core import serializers
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    places = Place.objects.all()
+    placesjson = serializers.serialize('json', Place.objects.all())
+
+    context = {
+        'places': places,
+        'placesjson': placesjson,
+    }
+    return render(request, 'main/index.html', context)
