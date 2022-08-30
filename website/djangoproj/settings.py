f2ad604a5401ca92a0896b8d722692eaf2684d72
yewon <yewon.ux@gmail.com>
2022-08-30 22:37:22 +0900

    django setting

--- website/djangoproj/settings.py
+++ website/djangoproj/settings.py
@@ -30,14 +30,13 @@ secret_file = os.path.join(BASE_DIR, 'secrets.json')
 with open(secret_file) as f:
     secrets = json.loads(f.read())
 
-def get_secret(setting):
+def get_secret(setting, secrets=secrets):
     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)    
-
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
