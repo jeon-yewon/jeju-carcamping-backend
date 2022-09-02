a5b8f23cd115709e5c4898a7dca72e27295377e7
Tae Hong Kim <kimthong2163@gmail.com>
2022-09-02 11:39:00 +0000

    최종

--- website/static/js/detail.js
+++ website/static/js/detail.js
@@ -1,5 +1,9 @@
 let roadmapBtn = document.querySelector('.show-roadmap')
 
 roadmapBtn.addEventListener('click', function() {
-    document.querySelector('#map').classList.toggle('show')
-})
+    document.querySelector('#map').classList.toggle('show');
+
+    roadmapBtn.innerText === "로드맵보기" ? 
+    roadmapBtn.innerText = "지도보기" : 
+    roadmapBtn.innerText = "로드맵보기";
+});
